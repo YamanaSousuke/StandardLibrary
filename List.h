2ae7eb8361f6fa1e01b855c6642d81a03db11e1d
YamanaSousuke <103532568+YamanaSousuke@users.noreply.github.com>
2023-02-11 13:03:01 +0900

    [add] Add Allocator class
    メモリを割り当てるテンプレートクラスの追加

--- List.h
+++ List.h
@@ -1,41 +1,103 @@
 #pragma once
 
-// ノード
-struct Node {
-	int value = 0;
-	Node* next = nullptr;
-	Node* prev = nullptr;
+#include <exception>
+#include <iostream>
+
+template<class T>
+class Allocator
+{
+public:
+	Allocator() { std::cout << "Allocator Init" << std::endl; }
+	// Allocator(const Allocator&) { }
+
+	// valueのアドレスを返す
+	// T* Address(T& value) const { return &value; }
+	// ストレージの確保
+	T* Alloacte(size_t num)
+	{
+		T* ret = nullptr;
+		const size_t size = num * sizeof(T);
+	
+		try {
+			ret = new T[size];
+		}
+		catch(const std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+		return ret;
+	}
+
+	void Dealloacte(T* p)
+	{
+		delete p;
+	}
+private:
 };
 
 // リスト
-class List
+template<class T, class Alloc = Allocator<T>>
+class MyList
 {
 public:
-	// 初期化
-	void Initialize();
-	// 指定したノードの直後に要素の挿入
-	void InsertAfter(Node* node, int data);
-	// 先頭に要素を追加
-	void push_front(int data);
-	// 末尾に要素を追加
-	void push_back(int data);
-	// データの表示
-	void ToString();
-	// ノードの各メンバに値を設定
-	void SetNode(Node* node, int data, Node* next, Node* prev);
-	// 指定したノードの削除
-	void Remove(Node* node);
-	// 先頭から要素を削除	
-	void pop_front();
-	// 末尾から要素を削除
-	void pop_back();
-	// コンテナが空かどうかを判定する
-	bool empty();
-	// 全要素削除
-	void clear();
+	// コンストラクタ
+	explicit MyList(const Alloc& alloc = Alloc()) : size(0), allocT(alloc)
+	{
+		CreateNewNode();
+	}
+
+	// デストラクタ
+	~MyList()
+	{
+		while (size > 0) {
+			pop_back();
+		}
+
+		allocNode.Dealloacte(node);
+	}
+
+	// 末尾にデータの追加
+	void push_back(const T& value)
+	{
+
+	}
+
+	// 末尾ノードの削除
+	void pop_back()
+	{
+		if (size) {
+			std::cout << "delete last element" << std::endl;
+		}
+	}
 private:
-	// 先頭ノードへのポインター
-	Node* head = nullptr;
-	// 着目ノードへのポインター
-	Node* current = nullptr;
+	// ノード
+	struct Node {
+		T data = 0;
+		Node* next = nullptr;
+		Node* prev = nullptr;
+	};
+
+	// ノード
+	Node* node;
+	// サイズ
+	size_t size;
+	
+	Alloc allocT;
+
+	// ノードの割り当て
+	Allocator<Node> allocNode;
+
+private:
+	// 新しいノードの生成
+	void CreateNewNode()
+	{
+		node = allocNode.Alloacte(1);
+		node->next = node;
+		node->prev = node;
+	}
+
+	// ノードの生成
+	void CreateNode(const T& value = T())
+	{
+		Node* newNode = allocNode.Allocate(1);
+	}
 };
