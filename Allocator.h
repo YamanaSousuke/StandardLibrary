69b31c03b5d56d02e6adf52c64db2c7fa2773346
YamanaSousuke <103532568+YamanaSousuke@users.noreply.github.com>
2023-02-14 10:43:22 +0900

    [add] Add allocator class
    メモリの確保と破棄を管理するAllocatorクラスの追加

---
+++ Allocator.h
@@ -0,0 +1,40 @@
+#pragma once
+
+template<class T>
+class MyAllocator
+{
+public:
+	MyAllocator() { }
+
+	// ストレージの確保
+	T* allocate(size_t num)
+	{
+		T* ret = nullptr;
+		const size_t size = num * sizeof(T);
+
+		try {
+			ret = new T[size];
+		}
+		catch (const std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+		return ret;
+	}
+
+	void construct(T* p, const T& value)
+	{
+		*p = value;
+	}
+
+	// 領域の解放
+	void deallocate(T* p)
+	{
+		delete p;
+	}
+
+	// 割り当てられたストレージ内のオブジェクトの破棄
+	void destroy(T* p)
+	{
+		p->~T();
+	}
+};
--- List.h
+++ List.h
@@ -3,45 +3,7 @@
 #include <exception>
 #include <iostream>
 #include "ListIterator.h"
-
-template<class T>
-class MyAllocator
-{
-public:
-	MyAllocator() { }
-
-	// ストレージの確保
-	T* allocate(size_t num)
-	{
-		T* ret = nullptr;
-		const size_t size = num * sizeof(T);
-	
-		try {
-			ret = new T[size];
-		}
-		catch(const std::exception& e) {
-			std::cerr << e.what() << std::endl;
-		}
-		return ret;
-	}
-
-	void construct(T* p, const T& value)
-	{
-		*p = value;
-	}
-
-	// 領域の解放
-	void deallocate(T* p)
-	{
-		delete p;
-	}
-
-	// 割り当てられたストレージ内のオブジェクトの破棄
-	void destroy(T* p)
-	{
-		p->~T();
-	}
-};
+#include "Allocator.h"
 
 // リスト
 template<class T, class Alloc = MyAllocator<T>>
@@ -197,6 +159,22 @@ public:
 
 		++size;
 		return --position;
+	}
+
+	// 1つの要素を破棄する
+	iterator erase(iterator position)
+	{
+		return erase(position, position.getNodePointer()->next);
+	}
+
+	// 範囲を指定して要素の破棄
+	iterator erase(iterator first, iterator last)
+	{
+		while (first != last) {
+			deleteNode((++first).getNodePointer()->prev);
+		}
+
+		return last;
 	}
 
 	// 要素の挿入(第2引数に個数の指定)
