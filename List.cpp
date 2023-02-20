65e7735dfd480d55b3d20394acf80ac4821c0969
YamanaSousuke <103532568+YamanaSousuke@users.noreply.github.com>
2023-02-20 14:24:53 +0900

    [add] Add move constructor and move equal operator
    ムーブコンストラクターとムーブ代入演算子の追加

--- List.cpp
+++ List.cpp
@@ -1,31 +1,17 @@
-#include <iostream>
-#include "List.h"
+// #include <iostream>
+// #include "List.h"
 #include "SharedPointer.h"
-#include <list>
-#include <memory>
+#include "UniquePtr.h"
+// #include <list>
+// #include <memory>
 
 int main()
 {
-	MySharedPointer<int> test1;
+	MyUniquePtr<int> test2(new int(1));
+	MyUniquePtr<int> test;
+	// *test = 10;
+	test = std::move(test2);
+	std::cout << "test value  : " << *test << std::endl;
 
-	// std::shared_ptr<int> sint;
-	// std::shared_ptr<float> sfloat;
-	// 
-	// sint = sfloat;
-
-	
-	{
-		MySharedPointer<int> test2(new int(0));
-		test1 = test2;
-	
-		*test2 += 10;
-		*test1 += 10;
-	}
-
-	std::cout << "before : " << test1.get() << std::endl;
-	test1.reset(new int(30));
-	std::cout << "after  : " << test1.get() << std::endl;
-
-	// std::cout << *test1 << std::endl;
 	return 0;
 }
